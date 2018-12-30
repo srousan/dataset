1129f32b5b2ccfa6a9a7814047763eb79513d462
kkofahe <kkofahe@gmail.com>
2018-12-30 17:04:40 -0500

    add read json file with all makes

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import json
 from search import Search
 
-s = Search("Toyota", "Camry" , "38002")
-s.search_items()
+
+with open("data/make.json", "r") as obj:
+        make_list = json.load(obj)
+
+with open("data/input_m&m.json", "r") as obj:
+        input = json.load(obj)
+
+makes_ids = list(make_list.keys())
+#print(makes_ids[0])
+print(make_list[makes_ids[0]])
+
+#print(input[0]["Name"])
+#s = Search("Toyota", "Camry" , "38002")
+#s.search_items()
