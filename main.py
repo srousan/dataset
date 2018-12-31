78b5f4742749deaf5898af3c6be9ffa320667d58
kkofahe <kkofahe@gmail.com>
2018-12-31 00:33:54 -0500

    main task to read json file and pass the params done, data for model and make is true data

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import json
 from search import Search
 
-
 with open("data/make.json", "r") as obj:
         make_list = json.load(obj)
 
-with open("data/input_m&m.json", "r") as obj:
-        input = json.load(obj)
+with open("data/model.json", "r") as obj:
+        model_list = json.load(obj)
+
+with open("data/zip_code.json", "r") as obj:
+        zip_code_list = json.load(obj)
 
 makes_ids = list(make_list.keys())
-#print(makes_ids[0])
-print(make_list[makes_ids[0]])
 
-#print(input[0]["Name"])
-#s = Search("Toyota", "Camry" , "38002")
-#s.search_items()
+for make in make_list:
+    for model in model_list[make]: 
+        for zip_code in zip_code_list:
+            s = Search(make_list[make], model["modelName"] , zip_code)
+            s.search_items()
+
